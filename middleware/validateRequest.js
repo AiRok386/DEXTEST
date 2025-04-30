774ff301f657888a3f958567521fa93616d5819c
AiRok386 <rahulthakur432001@gmail.com>
2025-05-01 04:25:41 +0530

    Update validateRequest.js

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,20 +1,11 @@
 // 📁 middleware/validateRequest.js
 
-const validateRequest = (requiredFields = []) => {
-    return (req, res, next) => {
-      const missingFields = requiredFields.filter(field => {
-        return !(field in req.body) || req.body[field] === '';
-      });
-  
-      if (missingFields.length > 0) {
-        return res.status(400).json({
-          error: `Missing required fields: ${missingFields.join(', ')}`
-        });
-      }
-  
-      next();
-    };
+module.exports = (fields = []) => {
+  return (req, res, next) => {
+    const missing = fields.filter(field => !req.body[field]);
+    if (missing.length > 0) {
+      return res.status(400).json({ message: `Missing fields: ${missing.join(', ')}` });
+    }
+    next();
   };
-  
-  module.exports = validateRequest;
-  
+};
